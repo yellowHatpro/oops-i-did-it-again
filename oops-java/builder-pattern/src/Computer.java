b71f79f99829281f7fd3f39f73e1410ee5099494
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-13 08:28:28 +0530

    checking symlinks, unrelated

---
+++ oops-java/builder-pattern/src/Computer.java
@@ -0,0 +1,5 @@
+public class Computer {
+    private String HDD;
+    private String SSD;
+    private String
+}
---
+++ oops-java/builder-pattern/src/PCPlan.java
@@ -0,0 +1,8 @@
+public interface PCPlan {
+    public void setCPU(String cpu);
+    public void setRAM(String ram);
+    public void setSSD(String ssd);
+    public void setKeyboard(String keyboard);
+    public void setMouse(String mouse);
+    public void setMonitor(String monitor);
+}
